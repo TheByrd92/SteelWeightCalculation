242cd7d50349e5f47f85b6f67951023ea0a24466
iByrd92 <iByrd92@yahoo.com>
2018-03-30 16:47:52 -0500

    Added SteelPart

---
+++ SteelWeightCalculation/SteelPart.cs
@@ -0,0 +1,11 @@
+﻿namespace SteelWeightCalculation
+{
+    public abstract class SteelPart
+    {
+        public abstract string descriptionExpression { get; set; }
+        public abstract double weight { get; set; }
+        public abstract double length { get; set; }
+        public abstract string fullDescription { get; set; }
+        public abstract void CalculateWeight();
+    }
+}
