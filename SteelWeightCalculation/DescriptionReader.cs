2006bbb2e8189ef95bef71d312ce2b996ad6cadf
iByrd92 <iByrd92@yahoo.com>
2018-03-30 15:21:55 -0500

    FirstCommit

---
+++ SteelWeightCalculation/DescriptionReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SteelWeightCalculation
+{
+    public class DescriptionReader
+    {
+        private Regex rgx;
+        private enum SteelParts
+        {
+
+        }
+
+        private bool IsKnownPart()
+        {
+            return false;
+        }
+        /// <summary>
+        /// Calculate a weight value for a part based on description.
+        /// </summary>
+        /// <param name="description"></param>
+        public void CalculateWeightFor(string description)
+        {
+
+        }
+    }
+}
