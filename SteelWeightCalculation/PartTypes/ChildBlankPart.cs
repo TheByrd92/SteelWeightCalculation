57b91b620fd50379d7562b9c58691641d89b5931
iByrd92 <iByrd92@yahoo.com>
2018-04-02 13:38:29 -0500

    Added a "template" child part class.

---
+++ SteelWeightCalculation/PartTypes/ChildBlankPart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SteelWeightCalculation.PartTypes
+{
+    public class ChildBlankPart : SteelPart
+    {
+        public override double weight { get { return weight; } set => weight = value; }
+        public override double length { get { return length; } set => length = value; }
+        public override string fullDescription { get { return fullDescription; } set => fullDescription = value; }
+
+        public override void AddChildrenToTotalWeight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CalculateWeight()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
