e6c3b2df45f05edcbb8a60fff81aced09098d9da
iByrd92 <iByrd92@yahoo.com>
2018-04-02 11:03:23 -0500

    Added ZShape

---
+++ SteelWeightCalculation/PartTypes/ZShape.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SteelWeightCalculation.PartTypes
+{
+    public class ZShape : SteelPart
+    {
+        public override string descriptionExpression { get { return "([zZ]\\d+[xX]\\d+[xX]\\d+\\/\\d+)|(\\d*[Zz].*[Xx].*[Gg][Aa])"; } set { } }
+        public override double weight { get { return weight; } set => weight = value; }
+        public override double length { get { return length; } set => length = value; }
+        public override string fullDescription { get { return fullDescription; } set => fullDescription = value; }
+
+        public override void CalculateWeight()
+        {
+            string regExMatch = descriptionExpression;
+            string matchedString = new Regex(regExMatch).Match(fullDescription).ToString();
+            string regExSplitMatch = "[A-Za-z]";
+            string[] dimensions = new Regex(regExSplitMatch).Split(matchedString);
+            //Should get three different numbers only
+            double width = 0.0;
+            double leg = 0.0;
+            double thickness = 0.0;
+            using (UnitConversion.FeetAndInches uc = new UnitConversion.FeetAndInches())
+            {
+                width = uc.InputNum(dimensions[(int)DimensionIndices.Width] + "\"");
+                leg = uc.InputNum(dimensions[(int)DimensionIndices.Legs] + "\"");
+                if (leg == -1) double.TryParse(dimensions[(int)DimensionIndices.Legs], out leg);
+                thickness = uc.GetGaugeInDecimal(dimensions[(int)DimensionIndices.Thickness]);
+            }
+            if (length <= 0 || width <= 0 || leg <= 0 || thickness <= 0)
+            {
+                weight = 0.0;
+                return;
+            }
+            weight = System.Math.Round(((((leg * 2) + width) * thickness) * length) * DescriptionReader.WEIGHT_OF_STEEL, 2);
+        }
+    }
+}
